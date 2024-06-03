3c315ef8f2a05fd8cb79ac9b5ca53a316a07f215
¨tonyplfx¨ <manuel_palafoxm@outlook.es>
2024-06-03 11:25:58 -0600

    Commit

---
+++ src/EjerciciosSencillos/ComparadorDeEnteros.java
@@ -0,0 +1,12 @@
+package EjerciciosSencillos;
+//Compara enteros utilizando Instrucciones if, operadores relacionales y de igualdades.
+
+import java.util.Scanner;
+
+public class ComparadorDeEnteros {
+    //El metodo main empieza la ejecución de la aplicacion de java
+    public static void main(String[] args) {
+
+    }
+
+}
