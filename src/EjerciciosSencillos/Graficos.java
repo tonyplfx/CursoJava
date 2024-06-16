be8294a90ac2253a70ede690cbb1e8bd36e52cf5
tonyplfx <manuel_palafoxm@outlook.es>
2024-06-16 02:21:11 -0600

    Ejercicios de programación

---
+++ src/EjerciciosSencillos/Graficos.java
@@ -0,0 +1,15 @@
+package EjerciciosSencillos;
+import java.util.Scanner;
+
+public class Graficos {
+
+    public static void main(String[] args) {
+        System.out.printf("*%n**%n***%n****%n%n");
+
+        System.out.print("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.print("****");
+        System.out.println("**");
+    }
+}
