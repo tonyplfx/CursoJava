b390001cb7e7bf51580609025f1f486a34e7a868
¨tonyplfx¨ <manuel_palafoxm@outlook.es>
2024-07-17 01:29:34 -0600

    Changes

--- src/EjerciciosSencillos/HelloWord.java
+++ src/EjerciciosSencillos/HelloWord.java
@@ -3,6 +3,7 @@ package EjerciciosSencillos;
 public class HelloWord{
     public static void main(String[] args) {
         System.out.printf("\n Hello word!\n");
+
         /*System.out.printf("Vamos a hacer un ciclo que cuente hasta 5: \n");
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
--- src/EjerciciosSencillos/SaludoApp.java
+++ src/EjerciciosSencillos/SaludoApp.java
@@ -1,9 +1,22 @@
 package EjerciciosSencillos;
 
+import java.util.Scanner;
+
 public class SaludoApp {
     public static void main(String[] args) {
-        String nombre = "Aquiles Saludos";
-        System.out.println("Bienvenido " + nombre);
+
+        double double1 = 20.00;
+        double double2 = 80.00;
+
+        double suma = (double1 + double2) * 100;
+        System.out.println("El resultado es el siguiente: "+ suma);
+        double reminder = suma % 40.00;
+        System.out.println("El residuo es el siguiente: "+ reminder);
+        boolean result = (reminder == 0) ? true : false;
+        System.out.println("El resultado es el siguiente: "+ result);
+
+        if(result)
+
     }
 }
 
