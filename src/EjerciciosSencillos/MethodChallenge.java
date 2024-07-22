cdfbdab230c869f22670b6907a731be92d467444
¨tonyplfx¨ <manuel_palafoxm@outlook.es>
2024-07-22 10:59:06 -0600

    git commit

---
+++ src/EjerciciosSencillos/MethodChallenge.java
@@ -0,0 +1,20 @@
+package EjerciciosSencillos;
+
+public class MethodChallenge {
+    public static void main(String[] args) {
+
+        //variables
+        int PlayersPosition;
+        char PlayersName;
+
+        //Methods
+
+        //Declaration of methods
+
+        public static void displayHighScorePosition(){
+
+        }
+
+
+    }
+}
