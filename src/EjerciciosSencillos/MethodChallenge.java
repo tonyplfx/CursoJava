215b8f398d77170b971ea495b606aaeb8169710f
¨tonyplfx¨ <manuel_palafoxm@outlook.es>
2024-07-22 11:28:53 -0600

    Challenge Methods

--- src/EjerciciosSencillos/MethodChallenge.java
+++ src/EjerciciosSencillos/MethodChallenge.java
@@ -3,18 +3,31 @@ package EjerciciosSencillos;
 public class MethodChallenge {
     public static void main(String[] args) {
 
-        //variables
-        int PlayersPosition;
-        char PlayersName;
 
         //Methods
+        int HighScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", HighScorePosition);
 
         //Declaration of methods
+    }
 
-        public static void displayHighScorePosition(){
+        public static void displayHighScorePosition(String PlayerName,int HighScorePosition){
+            System.out.printf(PlayerName + " managed to get into position " +HighScorePosition + " on the high score list");
+        }
 
+        public static int calculateHighScorePosition (int PlayerScore){
+
+            if (PlayerScore >= 1000){
+                return 1;
+            } else if (PlayerScore >= 500 && PlayerScore <= 1000){
+                return 2;
+            } else if (PlayerScore >= 100 && PlayerScore <= 500){
+                return 3;
+            } else {
+                return 4;
+            }
         }
 
 
-    }
+
 }
