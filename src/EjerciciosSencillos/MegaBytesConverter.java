3f720a306897d825dbfc1751d6185f8051be3efc
¨tonyplfx¨ <manuel_palafoxm@outlook.es>
2024-07-30 14:14:02 -0600

    MegaBytesConverter

---
+++ src/EjerciciosSencillos/MegaBytesConverter.java
@@ -0,0 +1,23 @@
+package EjerciciosSencillos;
+import java.util.Scanner;
+
+public class MegaBytesConverter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give us the Size in Kb to convert in Mb");
+        int kiloBytes = sc.nextInt();
+        printMegaBytesAndKilobytes(kiloBytes);
+
+    }
+    public static void printMegaBytesAndKilobytes(int kiloBytes){
+        if (kiloBytes < 0){
+            System.out.println("Invalid Number");
+        } else{
+            int megaBytes = kiloBytes/1024;
+            int remainder = kiloBytes % 1024;
+            System.out.println(kiloBytes +"Kb = " +megaBytes + " MB AND "+ remainder +"Kb" );
+        }
+        return;
+    }
+}
