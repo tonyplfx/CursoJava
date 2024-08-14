e39b64a6a8f5f7a7a49625a9b5b556e9002c47cb
¨tonyplfx¨ <manuel_palafoxm@outlook.es>
2024-08-13 19:10:20 -0600

    Inches to Centimeters

---
+++ src/JavaMasterClass/CentimetersConverter.java
@@ -0,0 +1,18 @@
+package JavaMasterClass;
+import java.util.Scanner;
+
+public class CentimetersConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give us your height in ft and inch");
+        double v = convertToCentimeters(sc.nextInt(), sc.nextInt());
+        System.out.printconvertToCentimeters(v);
+    }
+    public static double convertToCentimeters(double inches){
+       return inches *= 2.54;
+    }
+    public static double convertToCentimeters(int heightFt, int remFt){
+        int resFt = heightFt * 12;
+        return resFt + remFt;
+    }
+}
